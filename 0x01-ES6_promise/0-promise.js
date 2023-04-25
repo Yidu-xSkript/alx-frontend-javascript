beb07d2115b84b19108c2227bcd5ccfce82dd6fa
Yidu-xSkript <yidu95@gmail.com>
2023-04-25 14:11:09 +0300

    Just checking response from promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,10 @@
+export default function getResponseFromAPI() {
+  const _ = true;
+  return new Promise((resolve, reject) => {
+    if (_) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
+}
