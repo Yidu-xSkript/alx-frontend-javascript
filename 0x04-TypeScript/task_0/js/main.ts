ff0c324f7cb9645e5e77f82a059f7a22642124d3
Yidu-xSkript <yidu95@gmail.com>
2023-04-26 14:45:19 +0300

    Created an interface for student -> created 2 students which were added to a list -> showed the list on a table

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,42 @@
+interface Student {
+    firstName: string,
+    lastName: string,
+    age: number,
+    location: string
+}
+
+const studentA: Student = {
+	firstName: "Donald",
+	lastName: "Trump",
+	age: 79,
+	location: "USA"
+};
+
+const studentB: Student = {
+        firstName: "Ron",
+        lastName: "Desantis",
+        age: 47,
+        location: "USA"
+};
+
+const studentsList: Student[] = [studentA, studentB];
+
+const table = document.createElement('table');
+const tbody = document.createElement('tbody');
+
+table.appendChild(tbody);
+
+studentsList.forEach((student: Student): void => {
+    const tr = document.createElement('tr');
+    const firstName = document.createElement('td');
+    const location = document.createElement('td');
+
+    firstName.textContent = student.firstName;
+    location.textContent = student.location;
+
+    tr.appendChild(firstName);
+    tr.appendChild(location);
+    tbody.appendChild(tr);
+});
+
+document.body.appendChild(table);
