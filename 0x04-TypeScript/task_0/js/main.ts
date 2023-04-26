82afe49692e4ddd64b6e6fa7723407bb0d87b319
Yidu-xSkript <yidu95@gmail.com>
2023-04-26 15:12:27 +0300

    possible eslint err fix

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,22 +1,22 @@
 interface Student {
-    firstName: string,
-    lastName: string,
-    age: number,
-    location: string
+  firstName: string,
+  lastName: string,
+  age: number,
+  location: string
 }
 
 const studentA: Student = {
-    firstName: "Donald",
-    lastName: "Trump",
-    age: 79,
-    location: "USA"
+  firstName: "Donald",
+  lastName: "Trump",
+  age: 79,
+  location: "USA"
 };
 
 const studentB: Student = {
-    firstName: "Ron",
-    lastName: "Desantis",
-    age: 47,
-    location: "USA"
+  firstName: "Ron",
+  lastName: "Desantis",
+  age: 47,
+  location: "USA"
 };
 
 const studentsList: Student[] = [studentA, studentB];
@@ -27,16 +27,16 @@ const tbody = document.createElement('tbody');
 table.appendChild(tbody);
 
 studentsList.forEach((student: Student): void => {
-    const tr = document.createElement('tr');
-    const firstName = document.createElement('td');
-    const location = document.createElement('td');
+  const tr = document.createElement('tr');
+  const firstName = document.createElement('td');
+  const location = document.createElement('td');
 
-    firstName.textContent = student.firstName;
-    location.textContent = student.location;
+  firstName.textContent = student.firstName;
+  location.textContent = student.location;
 
-    tr.appendChild(firstName);
-    tr.appendChild(location);
-    tbody.appendChild(tr);
+  tr.appendChild(firstName);
+  tr.appendChild(location);
+  tbody.appendChild(tr);
 });
 
 document.body.appendChild(table);
